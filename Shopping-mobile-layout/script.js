e550fab75988f3bd031287dd131092d639d37825
Claenio <claenio.souza@gmail.com>
2021-10-27 17:13:21 -0300

    Eshop layout mobile

---
+++ Shopping-mobile-layout/script.js
@@ -0,0 +1,145 @@
+//ITEMS I
+
+//item 7
+{
+// Div item
+var item = document.createElement("Div");
+item.setAttribute("class", "item item7")
+
+
+// Link
+var link = document.createElement("a");
+link.setAttribute("class", "link");
+link.setAttribute("href", "#");
+item.appendChild(link);
+
+// H3
+var h3 = document.createElement("h3");
+h3.innerText = "R$ 450,00"
+link.appendChild(h3)
+
+// Link itm
+var itm= document.createElement("img");
+itm.setAttribute("class", "itm itm7");
+itm.setAttribute("src", "http://shopinfo.vteximg.com.br/arquivos/ids/1229489/1.png?v=637698168204570000");
+link.appendChild(itm);
+
+//Link Small
+var small = document.createElement("small")
+small.innerHTML = "Gabinete Gamer RGB Neologic Branco - NL-C301W Unica"
+link.appendChild(small);
+
+
+// Appending the div element to body
+document.getElementsByClassName("container")[0].appendChild(item);
+}
+
+//item 8
+{
+// Div item
+var item = document.createElement("Div");
+item.setAttribute("class", "item item7")
+
+
+// Link
+var link = document.createElement("a");
+link.setAttribute("class", "link");
+link.setAttribute("href", "#");
+item.appendChild(link);
+
+// H3
+var h3 = document.createElement("h3");
+h3.innerText = "R$ 470,00";
+link.appendChild(h3)
+
+// Link itm
+var itm= document.createElement("img");
+itm.setAttribute("class", "itm itm7");
+itm.setAttribute("src", "https://img.terabyteshop.com.br/produto/g/gabinete-gamer-redragon-tailgate-mid-tower-com-5-fans-vidro-temperado-black-s-fonte-gc-702_75473.png");
+link.appendChild(itm);
+
+//Link Small
+var small = document.createElement("small")
+small.innerHTML = "Gabinete Gamer Redragon Tailgate, Mid Tower, Com 5 Fans, Vidro Temperado, Black, Sem fonte, GC-702"
+link.appendChild(small);
+
+
+// Appending the div element to body
+document.getElementsByClassName("container")[0].appendChild(item);
+}
+
+//item 9
+{
+// Div item
+var item = document.createElement("Div");
+item.setAttribute("class", "item item7")
+
+
+// Link
+var link = document.createElement("a");
+link.setAttribute("class", "link");
+link.setAttribute("href", "#");
+item.appendChild(link);
+
+// h3
+var h3 = document.createElement("h3")
+h3.innerText = "R$ 14.000,00"
+link.appendChild(h3)
+
+// Link itm
+var itm= document.createElement("img");
+itm.setAttribute("class", "itm itm7");
+itm.setAttribute("src", "https://img.terabyteshop.com.br/produto/g/placa-de-video-palit-nvidia-geforce-rtx-3080ti-gamingpro-12gb-gddr6x-384bit-ned308t019kb-132aa_123399.png");
+link.appendChild(itm);
+
+//Link Small
+var small = document.createElement("small")
+small.innerHTML = "Placa de Vídeo Palit NVIDIA GeForce RTX 3080 Ti GamingPro, LHR, 12GB, GDDR6X, DLSS, Ray Tracing, NED308T019KB-132AA"
+link.appendChild(small);
+
+
+// Appending the div element to body
+document.getElementsByClassName("container")[0].appendChild(item);
+}
+
+//item 10
+{
+// Div item
+var item = document.createElement("Div");
+item.setAttribute("class", "item item7")
+
+
+// Link
+var link = document.createElement("a");
+link.setAttribute("class", "link");
+link.setAttribute("href", "#");
+item.appendChild(link);
+
+// h3
+var h3 = document.createElement("h3")
+h3.innerText = "R$ 4.000,00"
+link.appendChild(h3)
+
+// Link itm
+var itm= document.createElement("img");
+itm.setAttribute("class", "itm itm7");
+itm.setAttribute("src", "https://img.terabyteshop.com.br/produto/g/placa-de-video-asus-geforce-rtx-3060-12gb-gddr6-192bit-90yv0gb2-m0na00_117864.png");
+link.appendChild(itm);
+
+//Link Small
+var small = document.createElement("small")
+small.innerHTML = "Placa de Vídeo Asus GeForce RTX 3060 DUAL OC V2, LHR, 12GB, GDDR6, DLSS, Ray Tracing, 90YV0GB2-M0NA10 - V2"
+link.appendChild(small);
+
+
+// Appending the div element to body
+document.getElementsByClassName("container")[0].appendChild(item);
+}
+
+//ITEMS END
+
+
+
+/*
+https://qastack.com.br/programming/6840326/how-can-i-create-and-style-a-div-using-javascript
+*/
